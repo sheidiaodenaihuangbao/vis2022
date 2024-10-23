af70a8819568853a29c0972af77ff2d2c89d57f8
sheidiaodenaihuangbao <3158898150@qq.com>
2024-10-23 13:40:41 +0800

    1

---
+++ test_2024_10_18/源.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main() {
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int left = 0;
+//	int right = sz - 1;
+//	int k = 6;
+//	int falg = 0;
+//	while (left <= right) {
+//		int mid = left + (right - left) / 2;
+//		//int mid = (left + right) / 2;
+//		if (arr[mid] < k) {
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > k) {
+//			right=mid-1;
+//		}
+//		else {
+//			falg = 1;
+//			printf("答对了，是%d	下标是%d", arr[mid], mid);
+//			break;
+//		}
+//	}
+//	if (falg == 0) {
+//		printf("找不到");
+//	}
+//	return 0;
+//}
+int main() {
+
+	return 0;
+}
